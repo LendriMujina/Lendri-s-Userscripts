fbf5c8c304c74a7adacf569354cb0c6f49af22c2
Lendri <104964464+LendriMujina@users.noreply.github.com>
2024-11-03 03:11:28 -0500

    Create Neopets - HTML5 Pet Lookup Fix.user.js

---
+++ Neopets - HTML5 Pet Lookup Fix.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Neopets - HTML5 Pet Lookup Fix
+// @namespace    http://tampermonkey.net/
+// @version      2024-11-02
+// @description  Replaces the often-incomplete renders with static images.
+// @author       Lendri Mujina
+// @include      *://www.neopets.com/petlookup*
+// @include      *://www.neopets.com/pl_preview.phtml?pet=*
+// @icon         https://pets.neopets.com/cp/j58z8q5z/1/1.png
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+        var nameLocation = document.querySelector('[style="font-weight: bold; font-size: 18px;"]').textContent;
+        var petThe = nameLocation.lastIndexOf(" the ");
+        var petName = nameLocation.substr(0,petThe); //These two lines borrowed from old script
+        let imgContainer = document.querySelector("#CustomNeopetView.pet_image_container");
+        imgContainer.style = "width:400px;height:400px;border:1px solid #000;padding-top:0;background:url('http://pets.neopets.com/cpn/" + petName + "/1/7.png') center / cover no-repeat;";
+        document.querySelector("#CustomNeopetView > div").style = "display:none;";
+}
+ )();
